01283cb2667175734c4c4638371bc9a9d514b900
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 12:37:07 +0200

    Magiczne daty

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,25 @@
 package org.zoodooz;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("Podaj w formie liczby dowolny dzień(1-31): ");
+        int day = keyboard.nextInt();
+        System.out.println("Podaj w formie liczby dowolny miesiąc(1-12): ");
+        int month = keyboard.nextInt();
+        System.out.println("Podaj dowolne dwie ostatnie liczby roku(01-99): ");
+        int year = keyboard.nextInt();
+
+        if (day*month == year){
+            System.out.println("Podana data jest magiczna! ;) ");
+        } else {
+            System.out.println("Nie ma nic magicznego w tej dacie! :( ");
+        }
     }
 }
