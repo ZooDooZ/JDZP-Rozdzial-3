efd0ee1bf6e190ad5c3d064c836f8db4528862e4
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 13:09:22 +0200

    Program do wyboru restauracji

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,51 @@
 package org.zoodooz;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Czy któraś z osób jest na diecie wegetariańskiej? (tak/nie): ");
+        String answerVegetarian = keyboard.nextLine();
+
+        System.out.println("Czy któraś z osób jest na diecie wegańskiej? (tak/nie): ");
+        String answerVegan = keyboard.nextLine();
+
+        System.out.println("Czy któraś z osób jest na diecie bezglutenowej? (tak/nie): ");
+        String answerGluten = keyboard.nextLine();
+
+        System.out.println("Możecie wybrać sie do następujących restauracji: ");
+
+        if (answerVegetarian.equalsIgnoreCase("tak") && answerVegan.equalsIgnoreCase("tak")
+                && answerGluten.equalsIgnoreCase("tak")) {
+            System.out.println("Kawiarnia na Rogu,\nKuchnia u Szefa.");
+        } else if (answerVegetarian.equalsIgnoreCase("nie") && answerVegan.equalsIgnoreCase("nie")
+                && answerGluten.equalsIgnoreCase("nie")) {
+            System.out.println("Luksusowe Burgery u Jarka.");
+        } else if (answerVegetarian.equalsIgnoreCase("tak") && answerVegan.equalsIgnoreCase("nie")
+                && answerGluten.equalsIgnoreCase("tak")) {
+            System.out.println("Pizzeria przy Dworcowej,\nKawiarnia na Rogu,\nKuchnia u Szefa.");
+        } else if (answerVegetarian.equalsIgnoreCase("nie") && answerVegan.equalsIgnoreCase("tak")
+                && answerGluten.equalsIgnoreCase("nie")) {
+            System.out.println("Kawiarnia na Rogu,\nKuchnia u Szefa.");
+        } else if (answerVegetarian.equalsIgnoreCase("nie") && answerVegan.equalsIgnoreCase("tak")
+                && answerGluten.equalsIgnoreCase("tak")) {
+            System.out.println("Kawiarnia na Rogu,\nKuchnia u Szefa.");
+        } else if (answerVegetarian.equalsIgnoreCase("tak") && answerVegan.equalsIgnoreCase("nie")
+                && answerGluten.equalsIgnoreCase("nie")) {
+            System.out.println("Pizzeria przy Dworcowej,\nKawiarnia na Rogu,\nWłoskie Specjały,\nKuchnia u Szefa.");
+        }else if (answerVegetarian.equalsIgnoreCase("tak") && answerVegan.equalsIgnoreCase("tak")
+                && answerGluten.equalsIgnoreCase("nie")) {
+            System.out.println("Kawiarnia na Rogu,\nKuchnia u Szefa,\nKuchnia u Szefa.");
+        } else if (answerVegetarian.equalsIgnoreCase("nie") && answerVegan.equalsIgnoreCase("nie")
+                && answerGluten.equalsIgnoreCase("tak")) {
+            System.out.println("Pizzeria przy Dworcowej,\nKawiarnia na Rogu.");
+        } else {
+            System.out.println("WYPAD JEŚĆ GDZIE INDZIEJ!!!");
+        }
     }
 }
