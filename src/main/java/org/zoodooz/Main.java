f1871c9169eed6f4cf21a68d46ac30d1ce264624
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 13:07:07 +0200

    Punkty w klubie książki

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,36 @@
 package org.zoodooz;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Wprowadź liczbę zakupionych książek w danym miesiącu: ");
+        int numberOfBooks =  keyboard.nextInt();
+
+        switch (numberOfBooks) {
+            case 0:
+                System.out.println("Otrzymujesz 0 pkt.");
+                break;
+            case 1:
+                System.out.println("Otrzymujesz 5 pktów.");
+                break;
+            case 2:
+                System.out.println("Otrzymujesz 15 pktów.");
+                break;
+            case 3:
+                System.out.println("Otrzymujesz 30 pktów.");
+                break;
+            case 4:
+                System.out.println("Otrzymujesz 60 pktów.");
+                break;
+            default:
+                System.out.println("Otrzymujesz 60 pktów");
+        }
 
     }
 }
