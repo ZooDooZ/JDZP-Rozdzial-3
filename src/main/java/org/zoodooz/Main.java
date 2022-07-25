c54e38b71160f652d74d71b56be3f4a3a82b7425
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 12:42:57 +0200

    Kalkulator czasu

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,29 @@
 package org.zoodooz;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Proszę o wprowadzenie liczby sekund: ");
+        int seconds = keyboard.nextInt();
+
+        if (seconds < 60){
+            System.out.println(seconds + " sek.");
+        } else if (seconds >= 60 && seconds < 3600){
+            int minutes = seconds / 60;
+            System.out.println(minutes + " min.");
+        } else if (seconds >= 3600 && seconds < 86400){
+            int hours = seconds / 3600;
+            System.out.println(hours + " godz.");
+        } else if (seconds >=86400){
+            int days = seconds / 86400;
+            System.out.println(days + " dni");
+        } else {
+            System.out.println("Błąd wprowadzania sekund!");
+        }
     }
 }
