7ab600a2039d39cf4336b480ba90c546c7ad7f82
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 12:36:02 +0200

    Liczby rzymskie

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,51 @@
 package org.zoodooz;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Wpisz liczbę z przedziału od 1 do 10");
+        int number = keyboard.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("I");
+                break;
+            case 2:
+                System.out.println("II");
+                break;
+            case 3:
+                System.out.println("III");
+                break;
+            case 4:
+                System.out.println("IV");
+                break;
+            case 5:
+                System.out.println("V");
+                break;
+            case 6:
+                System.out.println("VI");
+                break;
+            case 7:
+                System.out.println("VII");
+                break;
+            case 8:
+                System.out.println("VIII");
+                break;
+            case 9:
+                System.out.println("IX");
+                break;
+            case 10:
+                System.out.println("X");
+                break;
+            default:
+                System.out.println("Błedna liczba!");
+            }
 
     }
 }
