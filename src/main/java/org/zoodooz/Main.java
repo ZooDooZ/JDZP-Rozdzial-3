d423e1e43d6a39f3497fda7f409c4db10df25cbf
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 12:59:39 +0200

    Udział w biegu

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,45 @@
 package org.zoodooz;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Podaj swoje nazwisko: ");
+        String surnamePlayer1 = keyboard.nextLine();
+        System.out.println("Podaj czas ukończenia biegu(min): ");
+        int timePlayer1 = keyboard.nextInt();
+
+        System.out.println("Podaj swoje nazwisko: ");
+        keyboard.nextLine();
+        String surnamePlayer2 = keyboard.nextLine();
+        System.out.println("Podaj czas ukończenia biegu(min): ");
+        int timePlayer2 = keyboard.nextInt();
+
+        System.out.println("Podaj swoje nazwisko: ");
+        keyboard.nextLine();
+        String surnamePlayer3 = keyboard.nextLine();
+        System.out.println("Podaj czas ukończenia biegu(min): ");
+        int timePlayer3 = keyboard.nextInt();
+
+        if (timePlayer1 < timePlayer2 && timePlayer2 < timePlayer3) {
+            System.out.println(surnamePlayer1 + "\n" + surnamePlayer2 + "\n" + surnamePlayer3);
+        } else if (timePlayer2 < timePlayer3 && timePlayer1 < timePlayer3) {
+            System.out.println(surnamePlayer2 + "\n" + surnamePlayer1 + "\n" + surnamePlayer3);
+        } else if (timePlayer3 < timePlayer1 && timePlayer2 < timePlayer1) {
+            System.out.println(surnamePlayer3 + "\n" + surnamePlayer2 + "\n" + surnamePlayer1);
+        } else if (timePlayer3 < timePlayer1 && timePlayer1 < timePlayer2) {
+            System.out.println(surnamePlayer3 + "\n" + surnamePlayer1 + "\n" + surnamePlayer2);
+        } else if (timePlayer1 < timePlayer3 && timePlayer3 < timePlayer2) {
+            System.out.println(surnamePlayer1 + "\n" + surnamePlayer3 + "\n" + surnamePlayer2);
+        } else if (timePlayer3 < timePlayer2 && timePlayer1 < timePlayer2) {
+            System.out.println(surnamePlayer2 + "\n" + surnamePlayer3 + "\n" + surnamePlayer1);
+        } else {
+            System.out.println("COŚ ZRĄBALEŚ! :D ");
+        }
     }
 }
