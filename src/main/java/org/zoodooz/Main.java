e1df8a661b7e3ccf9fce07883f33ac7143119ce4
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 12:41:08 +0200

    Masa i ciężar

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,23 @@
 package org.zoodooz;
 
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        String massObject = JOptionPane.showInputDialog("Prosze podac mase obiektu: ");
+        double massObjectD = Double.parseDouble(massObject);
+
+        double weigh = massObjectD * 9.8;
+
+        if (weigh > 1000){
+            System.out.println("Obiekt jest zbyt ciężki, przekracza 1000 niutonów i dokładnie wynosi: " + weigh);
+        } else if(weigh < 10){
+            System.out.println("Obiekt jest zbyt lekki, jest poniżej 10 niutonów i dokładnie wynosi: " + weigh);
+        } else {
+            System.out.println("Odpowiedni zakres ciężaru obiektu.");
+        }
     }
 }
